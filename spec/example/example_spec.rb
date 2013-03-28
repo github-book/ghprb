5ffdf571c1fb598097aa1dd0e6cea95ea6a401f0
yuria <hohtsuka+yuria@gmail.com>
2013-03-28 10:17:41 +0900

    Add test

--- spec/example/example_spec.rb
+++ spec/example/example_spec.rb
@@ -3,5 +3,8 @@ require 'rspec'
 describe 'example' do
   describe 'example1' do
     it { true.should be_true }
+    it { true.should be_true }
+    it { true.should be_true }
+    it { true.should be_true }
   end
 end
